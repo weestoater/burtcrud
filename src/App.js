6d56ac95862b957e2256f3dc05337da629468cc0
Ian Burrett <ian@weestoater.com>
2023-04-29 11:23:51 +0100

    added some narrative

--- src/App.js
+++ src/App.js
@@ -9,6 +9,15 @@ function App() {
         <h1>Hello from v2 of <code>burtcrud</code></h1>
         <p className="text-white"><i className="bi bi-person-circle"></i></p>
       </header>
+      <main className="container"> 
+        <div className="row">
+          <div className="col">
+            
+            <h2>What is the next step...</h2>
+            <p>We have a simple ReactJS app, now we need to give it a '<code>back-end</code>' for handling the queries to and from the <code>mongoDB</code>.</p>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
